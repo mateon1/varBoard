0fbe29735dbda2ac9ef4fb6b9fa5078488d3f7b7
bkucharz <bartosz_kucharz@outlook.com>
2022-03-31 23:38:38 +0200

    Start working on GUI, simple display of chessboard and pieces

---
+++ varboard/GUI/SquareView.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+import tkinter.messagebox
+
+
+class Square(tk.Frame):
+    def __init__(self, master, height, width, image=None):
+        def on_click(event):
+            tkinter.messagebox.showinfo(message=f'Clicked {self["bg"]} square')
+
+        super().__init__(master=master, height=height, width=width, relief='sunken', borderwidth=1)
+        self.piece = None
+        self.lbl_image = tk.Label(master=self, image=image, bg='linen')
+        self.lbl_image.bind('<Button-1>', on_click)
+        if image:
+            self.lbl_image.pack(expand=True, fill='both')
+        self.bind('<Button-1>', on_click)
+
+    def set_image(self, new_image):
+        self.lbl_image.configure(image=new_image)
+        self.lbl_image.pack(expand=True, fill='both')
+
+    def set_piece(self, piece):
+        self.piece = piece
+        self.set_image(piece.pimg)
+
+    def set_background(self, color):
+        self.configure(bg=color)
+        self.lbl_image.configure(bg=color)
