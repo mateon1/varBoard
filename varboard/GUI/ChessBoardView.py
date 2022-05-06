b6b3716aeb04796cd865e98c2e0b47207e611de7
Mateusz Naściszewski <matin1111@wp.pl>
2022-05-06 09:59:11 +0200

    Refactor promotion UI slightly

--- varboard/GUI/ChessBoardView.py
+++ varboard/GUI/ChessBoardView.py
@@ -12,7 +12,7 @@ class ChessBoardView(BoardView):
     def __init__(self, master, variant, square_scale, reverse=False, white_color='linen', black_color='saddle brown', *args, **kwargs):
         super().__init__(master, variant, square_scale, reverse, white_color, black_color, *args, **kwargs)
         self.last_clicked = None
-        self.promote_to = 'Q'
+        self.promotion_choice = None
 
     def set_bars(self, reverse):
         self.frm_number_bar = tk.Frame(master=self)
@@ -63,6 +63,17 @@ class ChessBoardView(BoardView):
             tkinter.messagebox.showinfo("Game over", message)
             self.master.end_game()
 
+    def do_promotion_move(self, moves):
+        PromotionWindow(self, [m.intopiece for m in moves]).wait_window()
+
+        for move in moves:
+            if move.intopiece == self.promotion_choice:
+                self.domove(move)
+                break
+        else:
+            raise ValueError("Failed to select promotion piece")
+        self.promotion_choice = None
+
     def handle_square_btn(self, square, x, y):
         print(f"Clicked {x}, {y}, last {self.last_clicked}")
         allmoves = []
@@ -78,12 +89,14 @@ class ChessBoardView(BoardView):
         if self.last_clicked:
             moves = [m for m in movesto if m.fromsq.to_tuple() == self.last_clicked]
         else:
-            # if len(set(m.fromsq for m in movesto)) == 1: # Unique move to
-            #     self.domove(movesto[0])
-            #     return
-            # elif len(set(m.tosq for m in movesfrom)) == 1: # unique move from
-            #     self.domove(movesfrom[0])
-            #     return
+            if len(set(m.fromsq for m in movesto)) == 1: # Unique move to
+                if len(movesto) > 1: self.do_promotion_move(movesto)
+                elif len(movesto): self.domove(movesto[0])
+                return
+            elif len(set(m.tosq for m in movesfrom)) == 1: # unique move from
+                if len(movesfrom) > 1: self.do_promotion_move(movesfrom)
+                elif len(movesfrom): self.domove(movesfrom[0])
+                return
 
             # first click, many options
             for m in movesfrom:
@@ -91,21 +104,13 @@ class ChessBoardView(BoardView):
             if len(movesfrom) > 0:
                 self.last_clicked = (x, y)
             return
-        if len(moves):
-            # if promotion
-            if len(moves) > 1:
-                PromotionWindow(self, Color.from_ply(self.controller.current.pos.ply)).wait_window()
 
-                for move in moves:
-                    if move.intopiece.ty == self.promote_to:
-                        self.domove(move)
-                        break
-            else:
-                self.domove(moves[0])
+        if len(moves) > 1: self.do_promotion_move(moves)
+        elif len(moves): self.domove(moves[0])
         self.last_clicked = None
 
-    def handle_promotion(self, chosen_piece : str):
-        self.promote_to = chosen_piece
+    def select_promotion(self, chosen_piece: str):
+        self.promotion_choice = chosen_piece
 
 
 if __name__ == '__main__':
--- varboard/GUI/promotion_window.py
+++ varboard/GUI/promotion_window.py
@@ -1,41 +1,49 @@
 import tkinter as tk
-from varboard.state import Color
+from queue import SimpleQueue
+from varboard.state import Color, Piece
 from varboard.GUI.widgets import PieceView
 from varboard.GUI.widgets import SquareView
 from varboard.GUI.BoardView import BoardView
 
 
 class PromotionWindow(tk.Toplevel):
-    def __init__(self, board_view : BoardView, color : Color):
+    def __init__(self, board_view: BoardView, selection: list[Piece]):
         super().__init__()
 
         self.board_view = board_view
 
         self.title('Promotion')
-        self.geometry('350x90')
+        #self.geometry('350x90')
 
         self.frm = tk.Frame(self)
         self.frm.pack()
-        self.piece_types = ['Q', 'N', 'R', 'B']
+        self.piece_types = selection
         self.pieces = []
         self.squares = []
+        self.sel_queue = SimpleQueue()
         for i, piece_type in enumerate(self.piece_types):
-            pimg = PieceView.load_image(f"../../pieces/{color.value+piece_type}.svg", (75, 75))
+            pimg = PieceView.load_image(f"pieces/{board_view.piece_to_id(piece_type)}.svg", (75, 75))
             self.squares.append(SquareView(self.frm, self, 75, 75, i, 0))
             self.pieces.append(PieceView(self.squares[-1], self, pimg))
             self.pieces[-1].set_xy(i, 0)
             self.pieces[-1].pack()
             self.squares[-1].pack(side=tk.LEFT)
 
-    def handle_square_btn(self,piece, x, y):
+    def handle_square_btn(self, piece, x, y):
         print(f"Clicked {x}, {y}")
-        self.board_view.handle_promotion(self.piece_types[x])
+        self.board_view.select_promotion(self.piece_types[x])
         self.destroy()
 
 
 if __name__ == '__main__':
-    root = PromotionWindow(None, Color.BLACK)
-    root.mainloop()
+    class MockBoardView:
+        def piece_to_id(self, p):
+            return BoardView.piece_to_id(self, p)
 
+        def select_promotion(self, p):
+            print("Selected promotion piece:", p)
+
+    root = PromotionWindow(MockBoardView(), [Piece(ty, Color.WHITE) for ty in "QNRB"])
+    root.mainloop()
 
 
